b3c68bc8e45ed61d02e6a13dd0867940af9780d2
Bryce Callender <brycec848@gmail.com>
2019-05-13 10:29:15 -0700

    comments

--- QuantumProgram/venv/Quantum.py
+++ QuantumProgram/venv/Quantum.py
@@ -6,14 +6,19 @@ print("Welcome to the random number guessing program")
 
 numOfQubits = int(input("How many qubits do you want to use? (1 for numbers 0 and 1, 2 for 0-3, etc...): "))
 
+# Make the quantum registers for generating the numbers and the
+# classical registers to grab the measured values
 q = QuantumRegister(numOfQubits)
 c = ClassicalRegister(numOfQubits)
 
-circuit = QuantumCircuit(q,c)
+# Create the quantum circuit
+circuit = QuantumCircuit(q, c)
 
+# Add numofQuibits amount of H gates to create superpositions
 for i in range(0, numOfQubits):
     circuit.h(q[i])
 
+# Measure every quantum register line
 circuit.measure(q, c)
 
 # Use Aer's qasm_simulator
@@ -28,6 +33,7 @@ counts = results.get_counts(circuit)
 mostCommon = 0
 highestBinary = ''
 
+# Find the highest probability for each state of bits
 for binary, value in counts.items():
     if value > mostCommon:
         mostCommon = value
@@ -39,6 +45,7 @@ guessed_num = int(highestBinary, 2)
 
 found = False
 
+# Try to guess the number
 while not found:
     num = int(input("Guess a number: "))
     if num == guessed_num:
